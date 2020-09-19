dbde6251206c2e0b51afc9cdf63806e2abe3706c
ali Sajadi <chargoonmobileintern2@outlook.com>
2020-09-19 13:15:44 +0430

    Modify WebSocketServer class and define basic websocket server functionality

--- src/WebSocketServer.java
+++ src/WebSocketServer.java
@@ -1,2 +1,35 @@
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@ServerEndpoint("/hello")
 public class WebSocketServer {
+
+    private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("Connected...");
+        sessions.add(session);
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        for (Session openSession : sessions) {
+            try {
+                openSession.getBasicRemote().sendText("I got your message");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("Disconnected...");
+        sessions.remove(session);
+    }
 }
